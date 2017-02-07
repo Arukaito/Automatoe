b781293fd4aa3af4f511ebb5d2713de46eb711c1
LDELAROSA <LDELAROSA@RAM-PC-7PF3K02>
2017-02-07 16:00:50 -0600

    Automatizacion de Dynalab

--- Automato/Automato.py
+++ Automato/Automato.py
@@ -4,23 +4,39 @@
 #  - 7z920-x64.msi is in the same folder as the script
 #  - UAC is fully disabled
 
+#Importamos Librerias
 from __future__ import print_function
 import sys, os
 os.chdir(os.path.join(os.getcwd(), os.path.dirname(sys.argv[0])))
 import pywinauto
 
+#Iniciamos la App de compilacion
 app = pywinauto.Application().Start(r'\\ramfile01\Apps\CompileNX\CompileNX.exe')
+
+#Hacemos Focus
 Compila = app['Compile NX electric test']
 
+#Declaramos el recurso donde nos indique que se compilara
 jobDir = r'C:\Users\LDELAROSA\Desktop\Jobs.txt'
 
+#Abrimos el archivo
 jobFile= open(jobDir, 'r+')
 #JobList= jobFile.readlines()
-            
+ 
+#Esperamos a que abra la ventana y que haga focus en el Edit3 que es el textbox
+tfrmmaincompilenx = app.TfrmMainCompileNX
+tfrmmaincompilenx.Wait('ready')
+tedit = tfrmmaincompilenx.Edit3
+
+#hacemos Loop por cada valor en la lista para que compile
 for index, line in enumerate(jobFile):
     job = line
     Compila.SetFocus()
-    Compila.TGroupBox.TEdit.Click()
+    tedit.DoubleClick()
     Compila.TypeKeys(job)
+    #Compila.SendKeys('{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}')
     Compila.Load_Job.Click()
     Compila.Compile.Click()
+   
+#Matamos la aplicacion p
+app.kill()
